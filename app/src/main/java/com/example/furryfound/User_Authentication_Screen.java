05e843620233250f9949f6b521b0cbc3ba197c43
Ivy Unabia Abapo <ivyabapo112102@gmail.com>
2023-11-15 16:14:08 +0800

    Authentication

--- app/src/main/java/com/example/furryfound/User_Authentication_Screen.java
+++ app/src/main/java/com/example/furryfound/User_Authentication_Screen.java
@@ -1,16 +1,31 @@
 package com.example.furryfound;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class User_Authentication_Screen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_authentication_screen);
-
-        
+        Button loginButton = findViewById(R.id.loginButton);
+        Button signUpButton = findViewById(R.id.signUpButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(User_Authentication_Screen.this, LogIn_Screen.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(User_Authentication_Screen.this, Register_Screen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
