5e217d8eed8bf5e32e7c11a6fef8155c77060c8f
Ivy Unabia Abapo <ivyabapo112102@gmail.com>
2023-11-24 20:21:26 +0800

    Notification_screen

---
+++ app/src/main/java/com/example/furryfound/NotificationScreen_Activity.java
@@ -0,0 +1,14 @@
+package com.example.furryfound;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_screen);
+    }
+}
