6a8776b34bf4e8752856364ffa69cc3ec04a34b9
TorrevillasJames <james.torrevillas@cit.edu>
2023-12-05 16:39:45 +0800

    push

--- app/src/main/java/com/example/furryfound/Screen_Dashboard.java
+++ app/src/main/java/com/example/furryfound/Screen_Dashboard.java
@@ -1,8 +1,15 @@
 package com.example.furryfound;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
 
 public class Screen_Dashboard extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class Screen_Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.NavbarView);
+        //bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            /*@Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        Intent intent = new Intent(Screen_Dashboard.this, Screen_Dashboard.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.favorite:
+                        intent = new Intent(Screen_Dashboard.this, FavoritesScreen_Activity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.notification:
+                        intent = new Intent(Screen_Dashboard.this, NotificationScreen_Activity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.settings:
+                        intent = new Intent(Screen_Dashboard.this, Settings_Activity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });*/
     }
 }
