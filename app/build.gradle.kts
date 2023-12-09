553576d86791dd95354df59d72cc1c738c92f6f7
Alyssa Vivien S. Cañas <alyssavivien.sc@gmail.com>
2023-12-09 22:21:31 +0800

    11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("com.makeramen:roundedimageview:2.3.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
@@ -43,18 +45,22 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation ("com.google.android.gms:play-services-auth:19.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.firebase:firebase-bom:32.6.0")
+    implementation("com.google.firebase:firebase-bom:32.7.0")
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
 
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.1")
 
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    implementation ("com.facebook.android:facebook-android-sdk:latest.release")
 }
--- app/src/main/java/com/example/furryfound/User_Class.java
+++ app/src/main/java/com/example/furryfound/User_Class.java
@@ -2,50 +2,71 @@ package com.example.furryfound;
 
 public class User_Class {
     String user_id;
-    String full_name;
+    String firstname;
+    String lastname;
     String address;
     String phone_number;
     String email;
+    String profile;
 
-    public User_Class(String full_name, String address, String phone_number, String email) {
-        this.full_name = full_name;
+    public User_Class(String firstname, String lastname, String address, String phone_number, String email, String profile) {
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.address = address;
         this.phone_number = phone_number;
         this.email = email;
+        this.profile = profile;
+    }
+
+    public User_Class() {
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public String getUser_id() {
         return this.user_id;
     }
-
-    public String getFull_name() {
-        return this.full_name;
-    }
-
     public String getAddress() {
         return this.address;
     }
-
     public String getPhone_number() {
         return this.phone_number;
     }
-
     public String getEmail() {
         return this.email;
     }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public void setPhone_number(String phone_number) {
         this.phone_number = phone_number;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
